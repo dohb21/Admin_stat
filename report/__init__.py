1061e2f7c23bbf371de67af4c433586990a9942e
도효빈 <dohb7133@castis.com>
2026-06-02 15:25:40 +0900

    markdown 삭제

--- report/__init__.py
+++ report/__init__.py
@@ -1,11 +1,9 @@
-from .markdown import build_markdown_report
 from .html import build_html_report
-from .email import build_email_html, build_report_image_html, send_email
+from .email import build_report_image_html, send_email
 from .kakao import build_kakao_text, send_kakao
 
 __all__ = [
-    "build_markdown_report",
     "build_html_report",
-    "build_email_html", "build_report_image_html", "send_email",
+    "build_report_image_html", "send_email",
     "build_kakao_text", "send_kakao",
 ]
